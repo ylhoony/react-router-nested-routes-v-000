6dded21affa278ac7ce390c2de00509fe72ea7b9
Hoon Lee <yl.hoony@gmail.com>
2018-06-13 00:30:35 -0700

    followed code along

---
+++ src/containers/MovieShow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MoviesShow = props => {
+
+  return (
+    <div>
+      <h3>Movies Show Component!</h3>
+    </div>
+  );
+}
+
+export default MoviesShow;
--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -1,11 +1,17 @@
 // ./src/containers/MoviesPage.js
 import React from 'react';
+import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import MoviesList from '../components/MoviesList';
+import MovieShow from './MovieShow';
 
-const MoviesPage = ({ movies }) => 
+const MoviesPage = ({ match, movies }) => 
   <div>
     <MoviesList movies={movies} />
+    <Route path={`${match.url}/:movieId`} component={MovieShow}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a Movie from the list.</h3>
+    )}/>
   </div>;
 
 const mapStateToProps = (state) => {
